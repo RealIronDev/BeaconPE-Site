9ca403ec85ac768fd1b538184b0fa0287d8d08b0
Philip Shilling <comedyman937@yahoo.com>
2014-08-13 12:07:25 -0700

    Create development.php

---
+++ development.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PocketBukkit is a free, open source server software for Minecraft: Pocket Edition written in Java.  It has a built-in API to allow you to customize your server in any way you can imagine.">
+    <title>PocketBukkit | Home</title>
+<?php include_once( 'includes/meta.php'); ?>
+</head>
+
+<body><font color="white">
+<?php include_once( 'includes/nav.php'); ?>
+    <!-- Masthead -->
+    <div class="masthead">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1><img src="img/logo.png" height="80" width="80"></img> PocketBukkit</h1>
+                    <h3></h3>
+                    <font size="4"><p>Your Server.  Your Way.</p></font>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Intro home page content -->
+    <div class="intro-marketing">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-40 col-md-40 intro-marketing-box">
+                    <u><h2>Development Build Help</h2></u>
+                    <p>A Development Build is an unstable build of a future version of PocketBukkit.  It is called a Development Build because it is recommended that the builds are only used by Developers of plugins and the software.  However, anyone is able to use these build on their server.  If you use one of these builds, no software developer is responisble for curruption of any files.</p>
+                  </br></br></br></br></br>
+                </div>
+              <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+    <!-- Page content -->
+<?php include_once( 'includes/js.php'); ?>
+</font>
+</body>
+</html>
