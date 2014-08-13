0f9db2ada4db9636a6eae8dd7f6bdd089ff59241
Philip Shilling <comedyman937@yahoo.com>
2014-08-13 12:06:52 -0700

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PocketBukkit is a free, open source server software for Minecraft: Pocket Edition written in Java.  It has a built-in API to allow you to customize your server in any way you can imagine.">
+    <title>PocketBukkit | WebChat</title>
+<?php include_once( 'includes/meta.php'); ?>
+</head>
+
+<body><font color="white">
+<?php include_once( 'includes/nav.php'); ?>
+    <!-- Masthead -->
+    <div class="masthead">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1><img src="img/logo.png" height="80" width="80"></img> PocketBukkit</h1>
+                    <h3></h3>
+                    <font size="4"><p>Your Server.  Your Way.</p></font>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Intro home page content -->
+    <div class="intro-marketing">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-40 col-md-40 intro-marketing-box">
+                    <u><h2><center>PocketBukkit WebChat</center></h2></u>
+                    <center><iframe src="http://webchat.freenode.net?channels=%23PocketBukkit&uio=d4" width="747" height="500"></iframe></center>
+                </div>
+              <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+    <!-- Page content -->
+<?php include_once( 'includes/js.php'); ?>
+</font>
+</body>
+</html>
