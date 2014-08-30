eb4d450dcc89d4bd5163fa752fc7f7b6fca886fb
Comedyman937 <comedyman937@yahoo.com>
2014-08-30 14:59:43 -0400

    Added BlockServer

---
+++ blockserver/index.php
@@ -0,0 +1,13 @@
+<HTML>
+	<head>
+		<title>BlockServer - Default Webpage</title>
+	</head>
+	<body>
+		<center>
+			<h1>It Works!</h1>
+			<p>
+				This is the default webpage for the BlockServer website.  The owner is currently setting something up for this area.
+			</p>
+		</center>
+	</body>
+</HTML>
