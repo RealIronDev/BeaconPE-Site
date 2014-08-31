ad16ef97fe3db9578e929fcce0733b19d2ce5203
Philip Shilling <comedyman937@yahoo.com>
2014-08-31 13:48:36 -0400

    Fixed updateSite.sh (Hopefully)

--- updateSite.sh
+++ updateSite.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 cd /var/www/html/
-wget https://github.com/PocketBukkit/PocketBukkit-Site/archive/master.zip
+wget https://github.com/PocketBukkit/BeaconPE-Site/archive/master.zip
 if [ -f ./master.zip ]; then
   unzip master.zip
   rm -rf master.zip
@@ -10,10 +10,11 @@ if [ -f ./master.zip ]; then
   fi
   rm -rf index.php
   rm -rf bootstrap/
+  rm -rf blockserver/
   rm -rf includes/
   rm -rf updateSite.sh
-  mv PocketBukkit-Site-master/* /var/www/html/
+  mv BeaconPE-Site-master/* /var/www/html/
   chmod 0700 updateSite.sh
-  rm -rf PocketBukkit-Site-master
+  rm -rf BeaconPE-Site-master
   rm -rf README.md
 fi
