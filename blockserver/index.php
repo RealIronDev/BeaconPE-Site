89ad0ac4470d136c2d493a42c8724e66996b48c4
Comedyman937 <comedyman937@yahoo.com>
2014-08-30 21:08:22 -0400

    Oops

--- blockserver/index.php
+++ blockserver/index.php
@@ -11,7 +11,7 @@
 	            		<span class="icon-bar"></span>
 	            		<span class="icon-bar"></span>
 	            	</button>
-	            	<a class="navbar-brand">PocketBukkit</a>
+	            	<a class="navbar-brand">BlockServer</a>
 	            </div>
 	            <div class="navbar-collapse collapse">
 	            	<ul class="nav navbar-nav">
