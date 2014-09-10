9e38ae906eca91393a724f783d14c97d76ede25b
Comedyman937 <comedyman937@yahoo.com>
2014-09-10 19:28:33 -0400

    Fixed Twitter Feed

--- pocketbukkit/includes/footer.php
+++ pocketbukkit/includes/footer.php
@@ -1,4 +1,17 @@
-    <div class="footer">
+	<div align="center">
+		<a href="https://twitter.com/PocketBukkit" class="twitter-follow-button" data-show-count="false" data-lang="it" data-size="large">Follow @PocketBukkit</a>
+		<script>
+			!function(d,s,id){
+				var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+				if(!d.getElementById(id)){
+					js=d.createElement(s);
+					js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);
+				}
+			}(document, 'script', 'twitter-wjs');
+		</script>
+	</div>
+
+	<div class="footer">
       <div class="container">
       	<p class="text-muted" align="right">Created by <a href="https://github.com/PocketBukkit">PocketBukkit</a></p>
       </div>
