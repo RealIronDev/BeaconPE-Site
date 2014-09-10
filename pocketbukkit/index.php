b326acbf1c9dc9159a41090082c85d5f03f2a8dc
Heromine <jonnimperato@yahoo.it>
2014-09-10 14:17:21 +0200

    Update index.php

--- pocketbukkit/index.php
+++ pocketbukkit/index.php
@@ -241,6 +241,7 @@
 		  </div>';
 	
 	include_once 'includes/footer.php';
+        //Twitter button
 	echo <div align="center"><a href="https://twitter.com/PocketBukkit" class="twitter-follow-button" data-show-count="false" data-lang="it" data-size="large">Follow @PocketBukkit</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 </div>
