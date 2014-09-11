b8bdc27edb08957f01d68c18a004c0f81a5227a7
Heromine <jonnimperato@yahoo.it>
2014-09-11 14:50:14 +0200

    Update footer.php

--- pocketbukkit/includes/footer.php
+++ pocketbukkit/includes/footer.php
@@ -10,9 +10,9 @@
 			}(document, 'script', 'twitter-wjs');
 		</script>
 	</div>
-<a href="https://twitter.com/share" class="twitter-share-button" data-text="Check this amazing software API" data-via="PocketBukkit" data-hashtags="pocketbukkit">Tweet</a>
+<div align="center">"<a href="https://twitter.com/share" class="twitter-share-button" data-text="Check this amazing software API" data-via="PocketBukkit" data-hashtags="pocketbukkit">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
+</div>
 	<div class="footer">
       <div class="container">
       	<p class="text-muted" align="right">Created by <a href="https://github.com/PocketBukkit">PocketBukkit</a></p>
