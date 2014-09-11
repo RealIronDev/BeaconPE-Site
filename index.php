784b4ade09fd253f42dc5dd21177b23cb23eb88d
Heromine <jonnimperato@yahoo.it>
2014-09-11 11:34:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -46,15 +46,16 @@
 
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_donations">
-<input type="hidden" name="business" value="account_email_needed">
-<input type="hidden" name="lc" value="IT">
+<input type="hidden" name="business" value="comedyman_email">
+<input type="hidden" name="lc" value="US">
 <input type="hidden" name="item_name" value="PocketBukkit">
 <input type="hidden" name="no_note" value="0">
-<input type="hidden" name="currency_code" value="EUR">
-<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
-<input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - Il metodo rapido, affidabile e innovativo per pagare e farsi pagare.">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 </td>
 </tr></div>
 </tbody>
