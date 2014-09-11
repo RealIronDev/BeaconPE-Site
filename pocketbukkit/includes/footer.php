710ceecad319b1a7c3a889adbb32a4ccaf607cec
Heromine <jonnimperato@yahoo.it>
2014-09-11 15:28:10 +0200

    Update footer.php
    Sorry for spam once again

--- pocketbukkit/includes/footer.php
+++ pocketbukkit/includes/footer.php
@@ -1,7 +1,6 @@
-	<div align="center">
-	<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/PocketBukkit" data-widget-id="510050434438791168">Tweets di @PocketBukkit</a>
+	<div align="center"><a class="twitter-timeline" href="https://twitter.com/PocketBukkit" data-widget-id="510050434438791168">Tweets di @PocketBukkit</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	<p><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://pocketbukkit.net/pocketbukkit/" data-text="Check out this amazing software API" data-via="PocketBukkit" data-size="large" data-hashtags="pocketbukkit">Tweet</a>
+<p><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://pocketbukkit.net/pocketbukkit/" data-text="Check out this amazing software API" data-via="PocketBukkit" data-size="large" data-hashtags="pocketbukkit">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></div>
   </body>
   <footer>
