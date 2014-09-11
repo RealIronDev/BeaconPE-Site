d6bbb86b3fbc2a5e2e4dccfacc18f6e11c52e97d
Heromine <jonnimperato@yahoo.it>
2014-09-11 16:09:16 +0200

    Update footer.php

--- pocketbukkit/includes/footer.php
+++ pocketbukkit/includes/footer.php
@@ -1,4 +1,4 @@
-	<div align="center"><a class="twitter-timeline" href="https://twitter.com/PocketBukkit" data-widget-id="510059667930619904">Tweets di @PocketBukkit</a>
+	<div align="center"><a class="twitter-timeline" href="https://twitter.com/PocketBukkit" data-widget-id="510067347579625472">Tweets di @PocketBukkit</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <p><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://pocketbukkit.net/pocketbukkit/" data-text="Check out this amazing software API" data-via="PocketBukkit" data-size="large" data-hashtags="pocketbukkit">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></div>
@@ -8,5 +8,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <p class="text-muted" align="right">Created by <a href="https://github.com/PocketBukkit">PocketBukkit</a></p>
   </footer>
 </html>
