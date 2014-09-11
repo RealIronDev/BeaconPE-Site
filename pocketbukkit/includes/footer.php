d12d44ba3b63ce99614d3662074d0b9108d78287
Heromine <jonnimperato@yahoo.it>
2014-09-11 15:20:10 +0200

    Update footer.php

--- pocketbukkit/includes/footer.php
+++ pocketbukkit/includes/footer.php
@@ -1,24 +1,9 @@
 	<div align="center">
-		<a href="https://twitter.com/PocketBukkit" class="twitter-follow-button" data-show-count="false" data-lang="en" data-size="large">Follow @PocketBukkit</a>
-		<script>
-			!function(d,s,id){
-				var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
-				if(!d.getElementById(id)){
-					js=d.createElement(s);
-					js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);
-				}
-			}(document, 'script', 'twitter-wjs');<a href="https://twitter.com/share" class="twitter-share-button" data-text="Check this amazing software API" data-via="PocketBukkit" data-hashtags="pocketbukkit">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
-		</script>
-	</div>
-	<div align="center"><a class="twitter-timeline" data-dnt="true" href="https://twitter.com/PocketBukkit" data-widget-id="">Tweets di @PocketBukkit</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
-      	<p class="text-muted" align="right">Created by <a href="https://github.com/PocketBukkit">PocketBukkit</a></p>
-      </div>
-    </div>
+	<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/PocketBukkit" data-widget-id="510050434438791168">Tweets di @PocketBukkit</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<p><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://pocketbukkit.net/pocketbukkit/" data-text="Check out this amazing software API" data-via="PocketBukkit" data-size="large" data-hashtags="pocketbukkit">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></div>
   </body>
-  
   <footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
