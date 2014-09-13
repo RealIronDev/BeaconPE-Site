576fce5ee4ff8eb638ba71b788b78a11009e313d
optrusty <optrusty@users.noreply.github.com>
2014-09-13 16:35:31 -0400

    Update 401.php

--- blockserver/ErrorDocs/401.php
+++ blockserver/ErrorDocs/401.php
@@ -9,12 +9,12 @@
     <link rel="icon" href="includes/img/favicon.ico">
     <title>BeaconPE - Error 404</title>
     <!-- Bootstrap core CSS -->
-    <link href="..../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="..../bootstrap/css/cover.css" rel="stylesheet">
+    <link href="../../bootstrap/css/cover.css" rel="stylesheet">
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="..../..../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="..../bootstrap/js/ie-emulation-modes-warning.js"></script>
+    <script src="../../bootstrap/js/ie-emulation-modes-warning.js"></script>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -52,9 +52,9 @@
     </div>
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="..../bootstrap/js/bootstrap.min.js"></script>
-    <script src="..../bootstrap/js/docs.min.js"></script>
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../../bootstrap/js/docs.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="..../bootstrap/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="../../bootstrap/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
