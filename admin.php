a75649c959caff63cf7f2d7187db938c4afc89a0
optrusty <optrusty@users.noreply.github.com>
2014-09-14 21:35:55 -0400

    Hello Staff!

--- admin.php
+++ admin.php
@@ -2,4 +2,7 @@
         $IPBlocker = new IPBlocker();
         $IP = array('68.100.171.85', '107.161.127.124', 'yet.another.ip');
 	$IPBlocker->acceptIP($IP);
+	
+
+	echo '<h3>Hello Staff!</h3>'
 ?>
