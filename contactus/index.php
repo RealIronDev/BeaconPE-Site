7a736f4e0120a0a387af52a5eeb82e66ca0d6c25
Heromine <jonnimperato@yahoo.it>
2014-09-19 15:16:07 +0200

    converted to php

---
+++ contactus/index.php
@@ -0,0 +1,138 @@
+echo '<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PocketBukkit | Server API</title>
+    <link rel="icon" type="image/x-icon" href="includes/img/favicon.ico">
+
+    <!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <style type="text/css"></style></head>
+  <body data-twttr-rendered="true"><div class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            			<span class="sr-only">Toggle navigation</span>
+	            		<span class="icon-bar"></span>
+	            		<span class="icon-bar"></span>
+	            		<span class="icon-bar"></span>
+	            	</button>
+	            	<a class="navbar-brand">Contact Us</a>
+	            </div>
+	            <div class="navbar-collapse collapse">
+	            	<ul class="nav navbar-nav">
+	            		<li><a data-toggle="modal" data-target="#email">Email </a></li>
+	            		<li><a data-toggle="modal" data-target="#irc">IRC </a></li>
+				<li><a data-toggle="modal" data-target="#twitter">Twitter </a></li>
+	            </div>
+			</div>
+	     </div><div class="container">
+			<div class="jumbotron">
+				<center>
+					<br>
+					<noscript>
+						&lt;div class="alert alert-danger"&gt;
+							This site uses JavaScript to run which is not enabled on your browser!
+						&lt;/div&gt;
+					</noscript>
+					<h1>PocketBukkit|BlockServer</h1><p><h1>The Team</h1></p>
+					<p>
+						Why we made it?
+					</p>
+					<br>
+					<hr>
+					<h1>Our team</h1>
+					<p>
+						Little description...
+<p>Api team: Name and emails</p>
+<p>Web team: Name and emails</p>
+<p>SoftWare team: Name and emails</p>
+<p>other idk</p>
+                      </p>
+				</center>
+			</div>
+  </div><div class="modal fade" id="email" tabindex="-1" role="dialog" aria-labelledby="emailLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+						<center><h4 class="modal-title" id="emailLabel">Email </h4></center>
+					</div>
+					<div class="modal-body">
+						<center>
+							<p>
+								Go to owner\'s E-mail?
+							</p>
+						</center>
+					</div>
+					<div class="modal-footer">
+						<center>
+							<a type="button" class="btn btn-success" href="mailto:comedymansemail@domain.com">Yes</a>
+							<button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+						</center>
+					</div>
+				</div>
+			</div>
+		  </div>
+		  </div><div class="modal fade" id="irc" tabindex="-1" role="dialog" aria-labelledby="ircLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+						<center><h4 class="modal-title" id="ircLabel">IRC</h4></center>
+					</div>
+					<div class="modal-body">
+						<center><iframe src="http://webchat.freenode.net?channels=%23PocketBukkit&amp;uio=d4" width="550" height="500"></iframe></center>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+					<div class="modal-footer">
+						<center>
+							<a type="button" class="btn btn-success" href="https://github.com/PocketBukkit">Yes</a>
+							<button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+						</center>
+					</div>
+				</div>
+			</div>
+		  </div><div class="modal fade" id="twitter" tabindex="-1" role="dialog" aria-labelledby="twitterLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+						<center><h4 class="modal-title" id="twitterLabel">Twitter </h4></center>
+					</div>
+					<div class="modal-body">
+						<center>
+							<p>
+								Go to owner\'s twitter account?
+							</p>
+						</center>
+					</div>
+					<div class="modal-footer">
+						<center>
+							<a type="button" class="btn btn-success" href="https://twitter.com/comedyman937">Yes</a>
+							<button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+						</center>
+					</div>
+				</div>
+			</div>
+  <footer>
+    <!-- jQuery (necessary for Bootstrap\'s JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <p class="text-muted" align="right">Created by <a href="https://github.com/PocketBukkit">PocketBukkit</a></p>
+  </footer>
+
+<iframe id="rufous-sandbox" scrolling="no" frameborder="0" allowtransparency="true" style="display: none;"></iframe></body></html>
+';
