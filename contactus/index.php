d3789943b2bc05c224a15a8eb4ef2b974b030bfc
optrusty <optrusty@users.noreply.github.com>
2014-09-19 20:48:56 -0400

    optrusty's contact

--- contactus/index.php
+++ contactus/index.php
@@ -53,7 +53,13 @@ echo '<html lang="en"><head>
 					<p>
 						Little description...
 <p>Api team: Name and emails</p>
-<p>Web team: Name and emails</p>
+<p>Web team: optrusty: optrusty, <a href="mailto:alemayhu21@gmail.com">Email optrusty</a> <a href="gtatf2gamer.tk">My Web Site</a> <style>.ig-b- { display: inline-block; }
+.ig-b- img { visibility: hidden; }
+.ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; }
+.ig-b-16 { width: 16px; height: 16px; background: url(//badges.instagram.com/static/images/ig-badge-sprite-16.png) no-repeat 0 0; }
+@media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
+.ig-b-16 { background-image: url(//badges.instagram.com/static/images/ig-badge-sprite-16@2x.png); background-size: 60px 178px; } }</style>
+<a href="http://instagram.com/gtatf2gamer?ref=badge" class="ig-b- ig-b-16"><img src="//badges.instagram.com/static/images/ig-badge-16.png" alt="Instagram" /></a> . Name and emails</p>
 <p>SoftWare team: Name and emails</p>
 <p>other idk</p>
                       </p>
