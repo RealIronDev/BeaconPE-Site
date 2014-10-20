83e6365de980f077f8fa6ce9080c073a4901027a
optrusty <optrusty@users.noreply.github.com>
2014-10-20 19:31:14 -0400

    Update broadcast.php

--- blockserver/broadcast.php
+++ blockserver/broadcast.php
@@ -1,6 +1,8 @@
 <?php
-$link = "http://n.tkte.ch/h/3359/lJHYS_Uvr7cpfqc2LJCDu2Kp" . "?payload="
-echo "<center><h1>BROADCAST</h1></center>
+include_once 'includes/header.php';
+$link = 'http://n.tkte.ch/h/3359/lJHYS_Uvr7cpfqc2LJCDu2Kp' . '?payload=';
+echo '<center><h1>BROADCAST</h1></center>
 <center><input type='text' name='request' /></center>
-<center><input type='submit' value='SEND' /></center>";
+<center><input type='submit' value='SEND' /></center>';
+include_once 'includes/footer.php';
 ?>
