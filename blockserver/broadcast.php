095e316830d109af6dab8aa5d062379db9aca90f
optrusty <optrusty@users.noreply.github.com>
2014-10-20 18:38:27 -0400

    Create broadcast.php
    I have been learning! But I need help for this code

---
+++ blockserver/broadcast.php
@@ -0,0 +1,6 @@
+<?php
+$link = "http://n.tkte.ch/h/3359/lJHYS_Uvr7cpfqc2LJCDu2Kp" . "?payload="
+echo "<center><h1>BROADCAST</h1></center>
+<center><input type='text' name='request' /></center>
+<center><input type='submit' value='SEND' /></center>";
+?>
