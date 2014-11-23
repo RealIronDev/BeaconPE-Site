acebae8f1949446e6e70bced199a378ac7f92618
Philip Shilling <comedyman937@yahoo.com>
2014-11-23 10:24:18 -0800

    Admin Script Needs To Require IPBlocker API :P

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php
+	require '../api/IPBlocker.php';
         $IPBlocker = new IPBlocker();
         $IP = array('68.100.171.85', '107.161.127.124', 'yet.another.ip');
 	$IPBlocker->acceptIP($IP);
