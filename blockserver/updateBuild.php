93d5cc1d460b23d1103e439d19b159f3fbeb9734
jython234 <jython234@users.noreply.github.com>
2014-11-24 17:36:32 -0600

    Updated building IOLib
    The compile will always fail, because java on the VPS is still java 6, not java 7. @Comedyman937, could you update it for me?

--- blockserver/updateBuild.php
+++ blockserver/updateBuild.php
@@ -1,6 +1,6 @@
 <?php
 
-$CLONE_URL = "https://github.com/BlockServerProject/IO-Lib.git";
+$CLONE_URL = "https://github.com/BlockServerProject/BlockServer-IO-Lib.git";
 
 $BUILD_DIR = "build";
 $DL_CMD = "cd $BUILD_DIR && git clone $CLONE_URL";
@@ -8,10 +8,14 @@ $BUILD_CMD = "ant compile";
 
 if( $_POST['payload']){ //Make sure its from github
   shell_exec($DL_CMD);
-  shell_exec("cd IO-Lib");
+  shell_exec("cd BlockServer-IO-Lib");
   $output = shell_exec($BUILD_CMD);
   if(strpos($output, "FAILED") !== false){
     //Success
+    echo("Build succeded.");
   } else {
      //Failure
+     echo("Build failed.");
   }
+  
+?>
